971dd2628e81140f71fd10f566296c094639cd6c
Chadwick Green <chadggreen@gmail.com>
2021-10-07 22:06:56 -0400

    idea table database services added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Page from './Components/Page';
 
 function App() {
   return (
     <div className="App">
-      
+      <Page/>
     </div>
   );
 }
---
+++ client/src/Services/IdeaServices.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const IDEA_URL = "http://localhost:8080/idea"
+
+class IdeaService {
+    getIdea = () =>{
+       return axios.get(IDEA_URL)
+    };
+    postIdea = (object) =>{
+        return axios.post(IDEA_URL, object)
+    };
+    patchIdea = (object) =>{
+        return axios.patch(IDEA_URL, object)
+    };
+    deleteIdea = (id) =>{
+        return axios.delete(IDEA_URL + id)
+    };
+}
+
+export default new IdeaService();
